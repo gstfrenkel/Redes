97b1e0911a2318bf5998f5369c777e6627745951
floritfran <fflorit@fi.uba.ar>
2024-04-19 17:05:55 -0300

    Agrego codigo simple para hacer un handshake entre cliente y servidor

--- TP1 - File Transfer/src/lib/client.py
+++ TP1 - File Transfer/src/lib/client.py
@@ -1,12 +1,64 @@
 from socket import *
+from lib.messages import SYN_MESSAGE, SYN_ACK_MESSAGE, CONNECTED_MESSAGE, FIN_MESSAGE, FIN_ACK_MESSAGE, DISCONNECTED_MESSAGE
+from lib.constants import TIMEOUT, MAX_SYN_TRIES, MAX_FIN_TRIES, MAX_MESSAGE_SIZE
 
 class Client:
     def __init__(self, serverAddress, serverPort):
         self.serverAddress = str(serverAddress)
         self.serverPort = int(serverPort)
+        self.connected = False
+        self.socket = None
+
+    def connect(self):
+        self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.socket.settimeout(TIMEOUT)
+
+        syn_tries = 0
+        while syn_tries < MAX_SYN_TRIES:
+            try:
+                self.socket.sendto(SYN_MESSAGE.encode(), (self.serverAddress, self.serverPort))
+                encondedMessage, serverSocket = self.socket.recvfrom(MAX_MESSAGE_SIZE)
+                decodedMessage = encondedMessage.decode()
+                
+                break
+            except timeout:
+                print("Timeout waiting for server ACK response. Retrying...")
+                syn_tries += 1
+
+        if syn_tries == MAX_SYN_TRIES:
+            self.socket.close()
+            self.socket = None
+            print("Maximum SYN tries reached")
+            raise KeyboardInterrupt
+
+        if decodedMessage == SYN_ACK_MESSAGE:
+            self.socket.sendto(CONNECTED_MESSAGE.encode(), (self.serverAddress, self.serverPort))
+            print("Connected to server")
+            self.connected = True
+
+    def disconnect(self):
+        fin_tries = 0
+        while fin_tries < MAX_FIN_TRIES:
+            try:
+                self.socket.sendto(FIN_MESSAGE.encode(), (self.serverAddress, self.serverPort))
+                encondedMessage, serverAddress = self.socket.recvfrom(MAX_MESSAGE_SIZE)
+                decodedMessage = encondedMessage.decode()
+
+                break
+            except socket.timeout:
+                print("Timeout waiting for server ACK response. Retrying...")
+                fin_tries += 1
+
+        if fin_tries == MAX_FIN_TRIES:
+            print("Maximum FIN tries reached")
+            raise KeyboardInterrupt
+
+        if decodedMessage == FIN_ACK_MESSAGE:
+            self.socket.sendto(DISCONNECTED_MESSAGE.encode(), (self.serverAddress, self.serverPort))
+            self.socket.close()
+            self.connected = False
+            print("Disconnected from server")
 
     def startUploading(self):
-        clientSocket = socket(AF_INET, SOCK_DGRAM)
-        message = '1|1|\\|\\' # primer 1 de upload y segundo de HI
-        clientSocket.sendto(message.encode(), (self.serverAddress, self.serverPort)) # aca habria que mandar el upload pero con el Hi
-        clientSocket.close() # en vez de terminar aca deberia empezar a escuchar si le responden del HI con un HI_ACK
+        message = '1|1|\\|\\' # primer 1 de upload y segundo de ACK
+        self.socket.sendto(message.encode(), (self.serverAddress, self.serverPort)) # aca habria que mandar el upload pero con una flag de conectado
--- TP1 - File Transfer/src/lib/server.py
+++ TP1 - File Transfer/src/lib/server.py
@@ -1,10 +1,13 @@
 from socket import *
 from threading import * 
+from lib.messages import SYN_MESSAGE, SYN_ACK_MESSAGE, CONNECTED_MESSAGE, FIN_MESSAGE, FIN_ACK_MESSAGE, DISCONNECTED_MESSAGE
+from lib.constants import MAX_MESSAGE_SIZE
 
 class Server:
     def __init__(self, address, port):
         self.address = str(address)
         self.port = int(port)
+        self.clients = {}
 
     def start(self):
         serverSocket = socket(AF_INET, SOCK_DGRAM)
@@ -15,14 +18,73 @@ class Server:
     def listen(self, serverSocket: socket):
         while True:
             try:
-                message, clientAddress = serverSocket.recvfrom(1024)
-                clientThread = Thread(target=self.handleClient, args=(clientAddress, message))
-                clientThread.start()
+                message, clientAddress = serverSocket.recvfrom(MAX_MESSAGE_SIZE)
+                decodedMessage = message.decode()
+                if decodedMessage == SYN_MESSAGE:
+                    self.processSynMessage(clientAddress)
+
+                elif decodedMessage == CONNECTED_MESSAGE:
+                    self.processSynOkMessage(clientAddress)
+
+                elif decodedMessage == FIN_MESSAGE:
+                    self.processFinMessage(clientAddress)
+
+                elif decodedMessage == DISCONNECTED_MESSAGE:
+                    self.processFinOkMessage(clientAddress)
+
+                else:
+                    print(decodedMessage)
+                    #handleClientMessage(message)
+                #clientThread = Thread(target=self.handleClient, args=(clientAddress, message))
+                #clientThread.start()
 
             except Exception as e:
                 serverSocket.close()
                 raise e
     
+    def processSynMessage(self, clientAddress):
+        print(f'SYN message arrived from client {clientAddress}, sending ACK...')
+        clientSocket = socket(AF_INET, SOCK_DGRAM)
+        clientSocket.settimeout(0.5)
+        
+        try:
+            clientSocket.sendto(SYN_ACK_MESSAGE.encode(), clientAddress)
+        except timeout:
+            print("Timeout waiting for client SYN_OK. Disconnecting...")
+
+        clientSocket.close()
+
+    def processSynOkMessage(self, clientAddress):
+        print(f'SYN_OK message arrived from client {clientAddress}. Connection stablished')
+        clientSocket = socket(AF_INET, SOCK_DGRAM)
+        clientSocket.settimeout(0.5)
+        
+        self.clients[clientAddress[1]] = clientSocket
+
+    def processFinMessage(self, clientAddress):
+        if clientAddress[1] in self.clients:
+            clientSocket = self.clients[clientAddress[1]]
+        else:
+            print(f'FIN message arrived from an unknown client')
+            return
+
+        try:
+            print(f'FIN message arrived from client {clientAddress}, sending ACK...')
+            clientSocket.sendto(FIN_ACK_MESSAGE.encode(), clientAddress)
+        except timeout:
+            print("Timeout waiting for client FIN_OK")
+
+    def processFinOkMessage(self, clientAddress):
+        if clientAddress[1] in self.clients:
+            clientSocket = self.clients[clientAddress[1]]
+        else:
+            print(f'FIN_OK message arrived from an unknown client')
+            return
+
+        clientSocket.close()
+        del self.clients[clientAddress[1]]
+        print(f'FIN_OK message arrived from client {clientAddress}. Disconnection successfully')
+
     def handleClient(self, clientAddress, message: bytes):
         decodedMessage = message.decode()
         print(f'Client {clientAddress} {decodedMessage}')
