26ac6cbae5b6c8273898a6e0dc2b47bbbabb948f
floritfran <fflorit@fi.uba.ar>
2024-06-13 16:45:44 -0300

    Corrijo reglas y error de dns

--- TP2_Openflow/firewall.py
+++ TP2_Openflow/firewall.py
@@ -12,16 +12,14 @@ from pox.lib.util import dpidToStr
 from pox.lib.revent.revent import EventMixin
 import pox.openflow.libopenflow_01 as of
 from pox.lib.packet.ethernet import ethernet
-import os
 import json
 ''' Add your imports here ... '''
 
 log = core.getLogger()
-policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ['HOME']
 
 ''' Add your global variables here ... '''
 FILE_NAME = "firewall_rules.json"
-TCP_PROTOCOL = 6
+protocols = {"TCP": 6, "UDP": 17, "ICMP": 1, "IGMP": 2}
 
 
 class Firewall(EventMixin):
@@ -32,28 +30,27 @@ class Firewall(EventMixin):
 
     def _handle_ConnectionUp(self, event):
         log.info(f"EVENT: {event.dpid}")
-        message = of.ofp_flow_mod()
 
         self.firewall_rules = self.get_firewall_rules(FILE_NAME)
         if event.dpid == self.firewall_rules['firewall_switch']:
-            self.set_firewall_rules(event, message)
+            self.set_firewall_rules(event)
             log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
 
-    def set_firewall_rules(self, event, message):
+    def set_firewall_rules(self, event):
         rules = self.firewall_rules['rules']
         for rule in rules:
+            message = of.ofp_flow_mod()
             message.match.dl_type = ethernet.IP_TYPE
             if "src_ip" in rule.keys():
                 message.match.nw_src = rule["src_ip"]
             if "dst_ip" in rule.keys():
                 message.match.nw_dst = rule["dst_ip"]
-            # De aca para abajo no funcionan muy bien, chequear la docu
             if "dst_port" in rule.keys():
                 if rule["dst_port"] == 80:
-                    message.match.nw_proto = TCP_PROTOCOL
+                    message.match.nw_proto = protocols["TCP"]
                 message.match.tp_dst = rule["dst_port"]
             if "protocol" in rule.keys():
-                message.match.nw_proto = rule["protocol"]
+                message.match.nw_proto = protocols[rule["protocol"]]
 
             event.connection.send(message)
 
